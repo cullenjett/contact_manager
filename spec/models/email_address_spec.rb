39c92f5cd9d21c3f3266f00aa9a48f2becc6c275
Cullen Jett <cullenjett@gmail.com>
2014-09-15 09:55:23 -0600

    Validate that an email address belongs to a person

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe EmailAddress, :type => :model do
   let(:email_address) do
-    EmailAddress.new(address: 'test@example.com')
+    EmailAddress.new(address: 'test@example.com', person_id: 1)
   end
 
   it "is valid" do
@@ -11,6 +11,11 @@ RSpec.describe EmailAddress, :type => :model do
 
   it "is invalid without an email address" do
     email_address.address = nil
+    expect(email_address).not_to be_valid
+  end
+
+  it "must have a reference to a person" do
+    email_address.person_id = nil
     expect(email_address).not_to be_valid
   end
 end
