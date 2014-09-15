652e64780f9adadf6fb0499f666966370764c98b
Cullen Jett <cullenjett@gmail.com>
2014-09-15 15:36:50 -0600

    Company validation tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Company, :type => :model do
+  let(:company) do
+    Company.create(name: 'Acme Corp')
+  end
 
+  it "is valid" do
+    expect(company).to be_valid
+  end
+
+  it "is invalid without a name" do
+    company.name = nil
+    expect(company).not_to be_valid
+  end
 end
