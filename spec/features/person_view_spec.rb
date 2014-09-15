461e428a011d453c784f7eb25b8a31e0779f742a
Cullen Jett <cullenjett@gmail.com>
2014-09-15 10:06:50 -0600

    Integration test for showing person's email addresses

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -7,59 +7,74 @@ describe 'the person view', type: :feature do
     Person.create(first_name: "Alice", last_name: "Smith")
   end
 
-  before(:each) do
-    alice.phone_numbers.create(number: '555-5555')
-    alice.phone_numbers.create(number: '555-1234')
-    visit person_path(alice)
-  end
+  describe 'phone numbers' do
+    before(:each) do
+      alice.phone_numbers.create(number: '555-5555')
+      alice.phone_numbers.create(number: '555-1234')
+      visit person_path(alice)
+    end
 
-  it "shows the phone numbers" do
-    alice.phone_numbers.each do |phone|
-      expect(page).to have_content(phone.number)
+    it "shows the phone numbers" do
+      alice.phone_numbers.each do |phone|
+        expect(page).to have_content(phone.number)
+      end
+    end
+
+    it "has a link to add a new phone number" do
+      expect(page).to have_link('Add phone number', href: new_phone_number_path(person_id: alice.id))
+    end
+
+    it "adds a new phone number" do
+      click_link('Add phone number')
+      fill_in('Number', with: '123-4567')
+      click_button('Create Phone number')
+      expect(current_path).to eq(person_path(alice))
+      expect(page).to have_content('123-4567')
+    end
+
+    it "has links to edit phone numbers" do
+      alice.phone_numbers.each do |phone|
+        expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+      end
+    end
+
+    it "edits a phone number" do
+      phone = alice.phone_numbers.first
+      old_number =  phone.number
+
+      first(:link, 'edit').click
+      fill_in 'Number', with: '555-9911'
+      click_button 'Update Phone number'
+      expect(current_path).to eq(person_path(alice))
+      expect(page).to have_content('555-9911')
+      expect(page).not_to have_content(old_number)
+    end
+
+    it "has a link to delete each phone number" do
+      expect(page).to have_link('delete')
+    end
+
+    it "deletes a phone number" do
+      phone = alice.phone_numbers.first
+      old_number = phone.number
+
+      first(:link, 'delete').click
+      expect(current_path).to eq(person_path(alice))
+      expect(page).to have_content(alice.first_name)
+      expect(page).not_to have_content(old_number)
     end
   end
 
-  it "has a link to add a new phone number" do
-    expect(page).to have_link('Add phone number', href: new_phone_number_path(person_id: alice.id))
-  end
-
-  it "adds a new phone number" do
-    click_link('Add phone number')
-    fill_in('Number', with: '123-4567')
-    click_button('Create Phone number')
-    expect(current_path).to eq(person_path(alice))
-    expect(page).to have_content('123-4567')
-  end
-
-  it "has links to edit phone numbers" do
-    alice.phone_numbers.each do |phone|
-      expect(page).to have_link('edit', href: edit_phone_number_path(phone))
+  describe 'email addresses' do
+    before do
+      alice.email_addresses.create(address: 'alice@example.com')
+      alice.email_addresses.create(address: 'lookingglass@wonderland.com')
+      visit person_path(alice)
     end
-  end
 
-  it "edits a phone number" do
-    phone = alice.phone_numbers.first
-    old_number =  phone.number
-
-    first(:link, 'edit').click
-    fill_in 'Number', with: '555-9911'
-    click_button 'Update Phone number'
-    expect(current_path).to eq(person_path(alice))
-    expect(page).to have_content('555-9911')
-    expect(page).not_to have_content(old_number)
-  end
-
-  it "has a link to delete each phone number" do
-    expect(page).to have_link('delete')
-  end
-
-  it "deletes a phone number" do
-    phone = alice.phone_numbers.first
-    old_number = phone.number
-
-    first(:link, 'delete').click
-    expect(current_path).to eq(person_path(alice))
-    expect(page).to have_content(alice.first_name)
-    expect(page).not_to have_content(old_number)
+    it "shows the email addresses" do
+      expect(page).to have_selector('li', text: 'alice@example.com')
+      expect(page).to have_selector('li', text: 'lookingglass@wonderland.com')
+    end
   end
 end
