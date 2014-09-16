b065215d306191e4d66c9e8b67c991b181d7b8a0
Cullen Jett <cullenjett@gmail.com>
2014-09-16 11:54:33 -0600

    Remove unecessary email addresses routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :people
   resources :phone_numbers, only: [:new, :create, :edit, :update, :destroy]
-  resources :email_addresses
+  resources :email_addresses, only: [:new, :create, :edit, :update, :destroy]
   resources :companies
 end
