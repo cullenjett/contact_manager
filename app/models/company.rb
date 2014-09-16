91ba467b6f12aa9a6fb999461982582d407e2ca8
Cullen Jett <cullenjett@gmail.com>
2014-09-16 12:04:42 -0600

    Extract contactable module for Person and Company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,5 @@
 class Company < ActiveRecord::Base
-  has_many :phone_numbers, as: :contact
-  has_many :email_addresses, as: :contact
+  include Contactable
 
   validates :name, presence: :true
 end
