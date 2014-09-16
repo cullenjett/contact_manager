fd613b7b39831830543f2d8bab77c4e828851799
Cullen Jett <cullenjett@gmail.com>
2014-09-16 11:53:15 -0600

    Remove uncecessary phone numbers specs and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :people
-  resources :phone_numbers
+  resources :phone_numbers, only: [:new, :create, :edit, :update, :destroy]
   resources :email_addresses
   resources :companies
 end
